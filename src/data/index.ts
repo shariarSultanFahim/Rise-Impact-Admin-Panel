d0bbcd83bbcba3592aa065916fe6299df4a4a592
shariarSultanFahim <shariarsfahim@gmail.com>
2026-03-31 00:23:50 +0600

    BUILD CHECK

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,5 @@
 export * from "./course-detail";
 export * from "./courses";
-export * from "./gamification";
 export * from "./notifications";
 export * from "./stack";
 export * from "./user-details";
